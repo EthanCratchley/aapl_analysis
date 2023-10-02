9d4afb5810ef3f0da4306e63404ae37e7d76aefb
Ethan <ethankcratchley@gmail.com>
2023-10-02 12:51:10 -0700

    cleaner version of code -> adding graphs etc. next

--- finance_analysis_3.py
+++ finance_analysis_3.py
@@ -2,206 +2,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('/Users/ethancratchley/desktop/appl_income_03.csv')
+# Function to calculate and print descriptive statistics
+def descriptive_statistics(df, index, metric_name):
+    data = df.iloc[1:, index]
+    data = pd.to_numeric(data, errors='coerce')
 
-# Transpose the DataFrame to swap rows with columns
+    mean = data.mean()
+    median = data.median()
+    std_dev = data.std()
+    min_value = data.min()
+    min_year = data.idxmin()
+    max_value = data.max()
+    max_year = data.idxmax()
+
+    print(f'Mean {metric_name}: ${mean:.2f}')
+    print(f'Median {metric_name}: ${median:.2f}')
+    print(f'Standard Deviation of {metric_name}: ${std_dev:.2f}')
+    print(f'Minimum {metric_name}: ${min_value:.2f} in the year {min_year}')
+    print(f'Maximum {metric_name}: ${max_value:.2f} in the year {max_year}')
+    print('-------------------------------------------------------------------------')
+
+# Read the data and transpose the DataFrame
+df = pd.read_csv('/Users/ethancratchley/desktop/appl_income_03.csv')
 df_transposed = df.T
 
-# Descriptive Statistics:
-descriptive_stats = []
+# Calculate descriptive statistics for different financial metrics
+metrics = [("Revenue", 0), ("COGS", 3), ("Gross Income", 6), ("Net Income", 29), ("Diluted Shares Outstanding", 39)]
 
-# -------------------------------------------------------------------------
-
-# Calculate Stats for Revenue:
-
-# Select Row:
-sales_data = df_transposed.iloc[1:, 0]
-
-# Convert the sales data to numeric, errors='coerce' will replace non-numeric values with NaN
-sales_data = pd.to_numeric(sales_data, errors='coerce')
-
-# Calculate the Stats
-mean_sales = sales_data.mean()
-median_sales = sales_data.median()
-std_sales = sales_data.std()
-
-# Get Min and Max with the corresponding year
-min_sales = sales_data.min()
-min_sales_year = sales_data.idxmin()
-max_sales = sales_data.max()
-max_sales_year = sales_data.idxmax()
-
-# Append to Stats List
-descriptive_stats.append(mean_sales)
-descriptive_stats.append(median_sales)
-descriptive_stats.append(std_sales)
-descriptive_stats.append(min_sales)
-descriptive_stats.append(min_sales_year)
-descriptive_stats.append(max_sales)
-descriptive_stats.append(max_sales_year)
-
-# Print Stats:
-print('Mean Revenue: ${:.2f}'.format(mean_sales))
-print('Median Revenue: ${:.2f}'.format(median_sales))
-print('Standard Deviation of Revenue: ${:.2f}'.format(std_sales))
-print(f"Minimum Revenue: ${min_sales:.2f} in the year {min_sales_year}")
-print(f"Maximum Revenue: ${max_sales:.2f} in the year {max_sales_year}")
-print('-------------------------------------------------------------------------')
-
-
-# -------------------------------------------------------------------------
-
-# Calculate Stats for COGS
-
-# Select Row:
-cogs_data = df_transposed.iloc[1:, 3]
-
-# Convert the COGS data to numeric, errors='coerce' will replace non-numeric values with NaN
-cogs_data = pd.to_numeric(cogs_data, errors='coerce')
-
-# Calculate the Stats
-mean_cogs = cogs_data.mean()
-median_cogs = cogs_data.median()
-std_cogs = cogs_data.std()
-
-# Get Min and Max with the corresponding year
-min_cogs = cogs_data.min()
-min_cogs_year = cogs_data.idxmin()
-max_cogs = cogs_data.max()
-max_cogs_year = cogs_data.idxmax()
-
-# Append to Stats List:
-descriptive_stats.append(mean_cogs)
-descriptive_stats.append(median_cogs)
-descriptive_stats.append(std_cogs)
-descriptive_stats.append(min_cogs)
-descriptive_stats.append(min_cogs_year)
-descriptive_stats.append(max_cogs)
-descriptive_stats.append(max_cogs_year)
-
-# Print Stats:
-print('Mean COGS: ${:.2f}'.format(mean_cogs))
-print('Median COGS: ${:.2f}'.format(median_cogs))
-print('Standard Deviation of COGS: ${:.2f}'.format(std_cogs))
-print(f"Minimum COGS: ${min_cogs:.2f} in the year {min_cogs_year}")
-print(f"Maximum COGS: ${max_cogs:.2f} in the year {max_cogs_year}")
-print('-------------------------------------------------------------------------')
-
-
-# -------------------------------------------------------------------------
-
-# Calculate Stats for Gross Income
-
-# Select Row:
-grossi_data = df_transposed.iloc[1:, 6]
-
-# Convert the Gross Income data to numeric, errors='coerce' will replace non-numeric values with NaN
-grossi_data = pd.to_numeric(grossi_data, errors='coerce')
-
-# Calculate the Stats
-mean_grossi = grossi_data.mean()
-median_grossi = grossi_data.median()
-std_grossi = grossi_data.std()
-
-# Get Min and Max with the corresponding year
-min_grossi = grossi_data.min()
-min_grossi_year = grossi_data.idxmin()
-max_grossi = grossi_data.max()
-max_grossi_year = grossi_data.idxmax()
-
-# Append to Stats List:
-descriptive_stats.append(mean_grossi)
-descriptive_stats.append(median_grossi)
-descriptive_stats.append(std_grossi)
-descriptive_stats.append(min_grossi)
-descriptive_stats.append(min_grossi_year)
-descriptive_stats.append(max_grossi)
-descriptive_stats.append(max_grossi_year)
-
-# Print Stats:
-print('Mean Gross Income: ${:.2f}'.format(mean_grossi))
-print('Median Gross Income: ${:.2f}'.format(median_grossi))
-print('Standard Deviation of Gross Income: ${:.2f}'.format(std_grossi))
-print(f"Minimum Gross Income: ${min_grossi:.2f} in the year {min_grossi_year}")
-print(f"Maximum Gross Income: ${max_grossi:.2f} in the year {max_grossi_year}")
-print('-------------------------------------------------------------------------')
-
-# -------------------------------------------------------------------------
-
-# Calculate Stats for Net Income
-
-# Select Row:
-neti_data = df_transposed.iloc[1:, 29]
-
-# Convert the Net Income data to numeric, errors='coerce' will replace non-numeric values with NaN
-neti_data = pd.to_numeric(neti_data, errors='coerce')
-
-# Calculate the Stats
-mean_neti = neti_data.mean()
-median_neti = neti_data.median()
-std_neti = neti_data.std()
-
-# Get Min and Max with the corresponding year
-min_neti = neti_data.min()
-min_neti_year = neti_data.idxmin()
-max_neti = neti_data.max()
-max_neti_year = neti_data.idxmax()
-
-# Append to Stats List:
-descriptive_stats.append(mean_neti)
-descriptive_stats.append(median_neti)
-descriptive_stats.append(std_neti)
-descriptive_stats.append(min_neti)
-descriptive_stats.append(min_neti_year)
-descriptive_stats.append(max_neti)
-descriptive_stats.append(max_neti_year)
-
-# Print Stats:
-print('Mean Net Income: ${:.2f}'.format(mean_neti))
-print('Median Net Income: ${:.2f}'.format(median_neti))
-print('Standard Deviation of Net Income: ${:.2f}'.format(std_neti))
-print(f"Minimum Net Income: ${min_neti:.2f} in the year {min_neti_year}")
-print(f"Maximum Net Income: ${max_neti:.2f} in the year {max_neti_year}")
-print('-------------------------------------------------------------------------')
-
-# -------------------------------------------------------------------------
-
-# Calculate Stats for Diluted Shares Outstanding
-
-# Select Row:
-shares_data = df_transposed.iloc[1:, 39]
-
-# Convert the Diluted Shares Outstanding data to numeric, errors='coerce' will replace non-numeric values with NaN
-shares_data = pd.to_numeric(shares_data, errors='coerce')
-
-# Calculate the Stats
-mean_shares = shares_data.mean()
-median_shares = shares_data.median()
-std_shares = shares_data.std()
-
-# Get Min and Max with the corresponding year
-min_shares = shares_data.min()
-min_shares_year = shares_data.idxmin()
-max_shares = shares_data.max()
-max_shares_year = shares_data.idxmax()
-
-# Append to Stats List:
-descriptive_stats.append(mean_shares)
-descriptive_stats.append(median_shares)
-descriptive_stats.append(std_shares)
-descriptive_stats.append(min_shares)
-descriptive_stats.append(min_shares_year)
-descriptive_stats.append(max_shares)
-descriptive_stats.append(max_shares_year)
-
-# Print Stats:
-print('Mean Diluted Shares Oustanding: ${:.2f}'.format(mean_shares))
-print('Median Diluted Shares Oustanding: ${:.2f}'.format(median_shares))
-print('Standard Deviation of Diluted Shares Oustanding: ${:.2f}'.format(std_shares))
-print(f"Minimum Diluted Shares Oustanding: ${min_shares:.2f} in the year {min_shares_year}")
-print(f"Maximum Diluted Shares Oustanding: ${max_shares:.2f} in the year {max_shares_year}")
-print('-------------------------------------------------------------------------')
-
+for metric_name, index in metrics:
+    descriptive_statistics(df_transposed, index, metric_name)
 
 
 

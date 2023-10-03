22a1f84030685891d238cc2f000cdc1da1ace2f0
Ethan <ethankcratchley@gmail.com>
2023-10-03 08:28:10 -0700

    fixing $ sign for shares

--- finance_analysis_3.py
+++ finance_analysis_3.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 # Function to calculate and print descriptive statistics
 def descriptive_statistics(df, index, metric_name):
+    global mean, median, std_dev, min_value, min_year, max_value, max_year
     data = df.iloc[1:, index]
     data = pd.to_numeric(data, errors='coerce')
 
@@ -22,16 +23,23 @@ def descriptive_statistics(df, index, metric_name):
     print(f'Maximum {metric_name}: ${max_value:.2f} in the year {max_year}')
     print('-------------------------------------------------------------------------')
 
+
 # Read the data and transpose the DataFrame
 df = pd.read_csv('/Users/ethancratchley/desktop/appl_income_03.csv')
 df_transposed = df.T
 
 # Calculate descriptive statistics for different financial metrics
-metrics = [("Revenue", 0), ("COGS", 3), ("Gross Income", 6), ("Net Income", 29), ("Diluted Shares Outstanding", 39)]
+metrics = [("Revenue", 0), ("COGS", 3), ("Gross Income", 6), ("Net Income", 29)]
 
 for metric_name, index in metrics:
     descriptive_statistics(df_transposed, index, metric_name)
 
-
+# Print Stats for Diluted Shares Outstanding:
+print('Mean Diluted Shares Outstanding: {:.2f}'.format(mean))
+print('Median Diluted Shares Outstanding: {:.2f}'.format(median))
+print('Standard Deviation of Diluted Shares Outstanding: {:.2f}'.format(std_dev))
+print(f"Minimum Diluted Shares Outstanding: {min_value:.2f} in the year {min_year}")
+print(f"Maximum Diluted Shares Outstanding: {max_value:.2f} in the year {max_year}")
+print('-------------------------------------------------------------------------')
 
 
